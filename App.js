76eea8d7b10b4b5739d6e9f2048b756dbc2afc3e
emre-yunus <r0664998@student.thomasmore.be>
2022-03-17 16:44:16 +0100

    opdracht 1.2

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {HomeScreen} from "./screens/HomeScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <HomeScreen/>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d07979',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import {Text, View} from "react-native";
+
+export function HomeScreen() {
+    return (
+        <View>
+            <Text>My Favourite Places</Text>
+        </View>
+    );
+}
