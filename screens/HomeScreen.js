2c1e6a86fe30e203bcb3186e6cb418bb6377b406
emre-yunus <r0664998@student.thomasmore.be>
2022-03-17 22:55:08 +0100

    opdracht 4.2

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,20 @@
 import {Text, View} from "react-native";
+import tw from "twrnc";
 
 export function HomeScreen() {
     return (
-        <View>
-            <Text>My Favourite Places</Text>
-        </View>
+        <>
+            <Banner/>
+            <View style={tw`h-full bg-yellow-300`}>
+            </View>
+        </>
     );
+}
+
+function Banner() {
+    return (
+        <View>
+            <Text style={tw`text-lg`}>My Favourite Places</Text>
+        </View>
+    )
 }
